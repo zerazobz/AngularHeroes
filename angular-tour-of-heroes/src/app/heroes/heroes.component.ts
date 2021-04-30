4b2c55f7d16ff2f97331e3b3dd366439e4554fe4
Erick Asto Oblitas <zerazobz@gmail.com>
2021-04-30 01:18:20 -0500

    Changing service to return data async

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -29,7 +29,8 @@ export class HeroesComponent implements OnInit {
   }
 
   getHeroes(): void {
-      this.heroes = this.heroService.getHeroes();
+      this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+      // this.heroes = this.heroService.getHeroes();
   }
 
 }
