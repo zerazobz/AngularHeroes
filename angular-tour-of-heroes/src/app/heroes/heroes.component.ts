912e875dd039d7d1b731088ceef6ca56ce1da978
Erick Asto Oblitas <zerazobz@gmail.com>
2021-04-30 01:31:46 -0500

    Addes message service, injected service in all services and components

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 // import { HEROES } from '../mock-heroes';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -17,7 +18,7 @@ export class HeroesComponent implements OnInit {
  //    };
 	// hero: String = "Windstorm"
 
-  constructor(private heroService: HeroService) {
+  constructor(private heroService: HeroService, private messageService: MessageService) {
   }
 
   ngOnInit(): void {
@@ -26,6 +27,7 @@ export class HeroesComponent implements OnInit {
 
   onSelect(hero: Hero): void {
       this.selectedHero = hero;
+      this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
