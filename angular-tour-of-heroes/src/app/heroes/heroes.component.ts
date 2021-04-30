d422066297abb85df04135f5d06a55b849b5d072
Erick Asto Oblitas <zerazobz@gmail.com>
2021-04-30 01:10:57 -0500

    Service added, and injected in HeroesComponent

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
-
+// import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,7 +9,7 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-    heroes = HEROES;
+    heroes: Hero[] = [];
     selectedHero? : Hero;
 	// hero: Hero = {
  //        id: 1,
@@ -17,15 +17,19 @@ export class HeroesComponent implements OnInit {
  //    };
 	// hero: String = "Windstorm"
 
-  constructor() {
-
+  constructor(private heroService: HeroService) {
   }
 
   ngOnInit(): void {
+      this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
       this.selectedHero = hero;
+  }
+
+  getHeroes(): void {
+      this.heroes = this.heroService.getHeroes();
   }
 
 }
