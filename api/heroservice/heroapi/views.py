2836d68f6fa92240f40801c4321bdf87bb300602
Erick Asto Oblitas <zerazobz@gmail.com>
2021-05-03 23:32:17 -0500

    Hero Service, basic

---
+++ api/heroservice/heroapi/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+# from api.heroservice.heroapi.serializers import UserSerializer, GroupSerializar
+from .serializers import UserSerializer, GroupSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_class = [IsAuthenticated]
+
+class GroupViewSet(viewsets.ModelViewSet):
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_class = [IsAuthenticated]
+        
